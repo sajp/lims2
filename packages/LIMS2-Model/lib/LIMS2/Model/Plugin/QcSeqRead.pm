5ea3f5aba51119fcff7e9fc625f37d6121f7d748
Sajith Perera <sajith.perera@gmail.com>
2012-04-02 14:04:00 +0100

    qc seq read plugin, create and retrieve functions

---
+++ packages/LIMS2-Model/lib/LIMS2/Model/Plugin/QcSeqRead.pm
@@ -0,0 +1,74 @@
+package LIMS2::Model::Plugin::QcSeqRead;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose::Role;
+use Hash::MoreUtils qw( slice slice_def );
+use Scalar::Util qw( blessed );
+use namespace::autoclean;
+
+requires qw( schema check_params throw );
+
+sub _instantiate_qc_seq_read {
+    my ( $self, $params ) = @_;
+
+    if ( blessed( $params ) and $params->isa( 'LIMS2::Model::Schema::Result::QcSeqRead' ) ) {
+        return $params;
+    }
+
+    my $validated_params = $self->check_params(
+        { slice( $params, qw( qc_seq_read_id ) ) },
+        { qc_seq_read_id => { validate => 'qc_seq_read_id' } }
+    );
+
+    $self->retrieve( QcSeqRead => $validated_params );
+}
+
+sub pspec_create_qc_seq_read {
+    return {
+        qc_seq_read_id        => { validate => 'qc_seq_read_id' },
+        qc_sequencing_project => { validate => 'plate_name' },
+        description           => { validate => 'non_empty_string' },
+        seq                   => { validate => 'dna_seq' },
+        length                => { validate => 'integer' },
+    };
+}
+
+sub create_qc_seq_read {
+    my ( $self, $params , $qc_sequencing_project ) = @_;
+
+    my $validated_params = $self->check_params( $params, $self->pspec_create_qc_seq_read );
+
+    $qc_sequencing_project ||= $self->_instantiate_qc_sequencing_project( $validated_params );
+
+    my $qc_seq_read = $qc_sequencing_project->create_related(
+        qc_seq_reads => {
+            slice_def( $validated_params, qw( qc_seq_read_id description seq length ) ),
+        }
+    );
+
+    $self->log->debug( 'created qc_seq_read with id: ' . $qc_seq_read->qc_seq_read_id );
+
+    return $qc_seq_read;
+}
+
+sub pspec_retrieve_qc_seq_read {
+    return {
+        qc_seq_read_id => { validate => 'qc_seq_read_id' },
+    };
+}
+
+sub retrieve_qc_seq_read {
+    my ( $self, $params ) = @_;
+
+    my $validated_params = $self->check_params( $params, $self->pspec_retrieve_qc_seq_read );
+
+    my $qc_seq_read = $self->retrieve( QcSeqRead => $validated_params );
+
+    return $qc_seq_read;
+}
+
+1;
+
+__END__
