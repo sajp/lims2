75ca1738fde25e24963c6d48980e4cda0aeaf96a
Sajith Perera <sajith.perera@gmail.com>
2012-04-02 10:44:02 +0100

    add create qc_sequencing_project plus tests

---
+++ packages/LIMS2-Model/lib/LIMS2/Model/Plugin/QcSequencingProject.pm
@@ -0,0 +1,48 @@
+package LIMS2::Model::Plugin::QcSequencingProject;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose::Role;
+use Hash::MoreUtils qw( slice slice_def );
+use Scalar::Util qw( blessed );
+use namespace::autoclean;
+
+requires qw( schema check_params throw );
+
+sub _instantiate_qc_sequencing_project {
+    my ( $self, $params ) = @_;
+
+    if ( blessed( $params ) and $params->isa( 'LIMS2::Model::Schema::Result::QcSequencingProject' ) ) {
+        return $params;
+    }
+
+    my $validated_params = $self->check_params(
+        { slice( $params, qw( qc_sequencing_project ) ) },
+        { qc_template_id => { validate => 'plate_name' } }
+    );
+
+    $self->retrieve( QcSequencingProject => $validated_params );
+}
+
+sub pspec_create_qc_sequencing_project {
+    return {
+        qc_sequencing_project => { validate => 'plate_name' },
+    };
+}
+
+sub create_qc_sequencing_project {
+    my ( $self, $params ) = @_;
+
+    my $validated_params = $self->check_params( $params, $self->pspec_create_qc_sequencing_project );
+
+    my $qc_sequencing_project = $self->schema->resultset( 'QcSequencingProject' )->create(
+        { slice_def( $validated_params, qw( qc_sequencing_project ) ) }
+    );
+
+    return $qc_sequencing_project;
+}
+
+1;
+
+__END__
