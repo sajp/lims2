cb149732cd02293ce14239638c9caa3cc8e245f5
Sajith Perera <sajith.perera@gmail.com>
2012-04-11 14:36:00 +0100

    move delete well function into plugin

--- packages/LIMS2-Model/lib/LIMS2/Model/Plugin/Well.pm
+++ packages/LIMS2-Model/lib/LIMS2/Model/Plugin/Well.pm
@@ -8,6 +8,7 @@ use Hash::MoreUtils qw( slice slice_def );
 use Scalar::Util qw( blessed );
 use List::MoreUtils qw( before_incl );
 use namespace::autoclean;
+use LIMS2::Model::Error::Database;
 
 requires qw( schema check_params throw );
 
@@ -19,7 +20,7 @@ sub check_parent_plate_type {
     for my $pw ( @{ $parent_wells } ) {
         $self->check_params( { plate_type => $pw->plate->plate_type },
                              { plate_type => { validate => $constraint } } );
-    }    
+    }
 }
 
 sub pspec_create_well {
@@ -49,7 +50,7 @@ sub _instantiate_well {
         { slice( $params, qw( plate_name well_name ) ) },
         { plate_name => {}, well_name  => {} }
     );
-    
+
     $self->retrieve( Well => $validated_params, { join => 'plate', prefetch => 'plate' } );
 }
 
@@ -58,9 +59,9 @@ sub _create_tree_paths {
     my ( $self, $well, @ancestors ) = @_;
 
     my ( $insert_tree_paths, @bind_params );
-    
+
     if ( @ancestors ) {
-        $self->check_parent_plate_type( $well, \@ancestors );    
+        $self->check_parent_plate_type( $well, \@ancestors );
         $insert_tree_paths = sprintf( <<'EOT', join q{, }, ('?')x@ancestors );
 insert into tree_paths( ancestor, descendant, path_length )
   select t.ancestor, cast( ? as integer ), t.path_length + 1
@@ -69,15 +70,15 @@ insert into tree_paths( ancestor, descendant, path_length )
 union all
   select cast( ? as integer ), cast( ? as integer ), 0
 EOT
-        @bind_params = ( $well->well_id, map( $_->well_id, @ancestors ), $well->well_id, $well->well_id ); 
-    }    
+        @bind_params = ( $well->well_id, map( $_->well_id, @ancestors ), $well->well_id, $well->well_id );
+    }
     else {
         $insert_tree_paths = <<'EOT';
 insert into tree_paths( ancestor, descendant, path_length ) values( ?, ?, 0 )
 EOT
         @bind_params = ( $well->well_id, $well->well_id );
     }
-    
+
     $self->schema->storage->dbh_do(
         sub {
             my $sth = $_[1]->prepare_cached( $insert_tree_paths );
@@ -108,13 +109,13 @@ sub _create_well {
     if ( $validated_params->{pipeline} ) {
         $process->create_related(
             process_pipeline => { pipeline_id => $validated_params->{pipeline} }
-        );        
+        );
     }
 
     for my $assay_result ( @{ $validated_params->{assay_results} } ) {
         $self->add_well_assay_result( $assay_result, $well );
-    }    
-    
+    }
+
     if ( $validated_params->{assay_complete} ) {
         $self->set_well_assay_complete( { assay_complete => $validated_params->{assay_complete} }, $well );
     }
@@ -122,7 +123,7 @@ sub _create_well {
     if ( $validated_params->{accepted} ) {
         $self->set_well_accepted_override( $validated_params->{accepted}, $well );
     }
-    
+
     return $well;
 }
 
@@ -139,13 +140,13 @@ sub pspec_set_well_assay_complete {
 sub set_well_assay_complete {
     my ( $self, $params, $well ) = @_;
 
-    $well ||= $self->_instantiate_well( $params );    
-    
+    $well ||= $self->_instantiate_well( $params );
+
     my $validated_params = $self->check_params( { slice_def( $params, 'assay_complete' ) },
                                                 $self->pspec_set_well_assay_complete );
 
     # XXX fire trigger to set 'accepted' flag
-    
+
     $well->update( { assay_complete => $validated_params->{assay_complete} } );
 }
 
@@ -154,13 +155,13 @@ sub pspec_set_well_accepted_override {
         accepted   => { validate => 'boolean' },
         created_by => { validate => 'existing_user', post_filter => 'user_id_for' },
         created_at => { validate => 'date_time', post_filter => 'parse_date_time' }
-    };    
+    };
 }
 
 sub set_well_accepted_override {
     my ( $self, $params, $well ) = @_;
 
-    $well ||= $self->_instantiate_well( $params );    
+    $well ||= $self->_instantiate_well( $params );
 
     my $validated_params = $self->check_params(
         { slice_def( $params, qw( accepted created_by created_at ) ) },
@@ -169,8 +170,8 @@ sub set_well_accepted_override {
 
     unless ( $well->assay_complete ) {
         $self->throw( InvalidState => 'Cannot override accepted unless assay_complete' );
-    }    
-    
+    }
+
     $well->search_related_rs( 'well_accepted_override' )->delete;
 
     my $accepted_override = $well->create_related( well_accepted_override => $validated_params );
@@ -186,14 +187,14 @@ sub pspec_add_well_assay_result {
         result     => { validate => 'existing_assay_result' },
         created_by => { validate => 'existing_user', post_filter => 'user_id_for' },
         created_at => { validate => 'date_time', post_filter => 'parse_date_time' }
-    };    
+    };
 }
 
 sub add_well_assay_result {
     my ( $self, $params, $well ) = @_;
 
     $well ||= $self->_instantiate_well( $params );
-    
+
     my $validated_params = $self->check_params(
         { slice_def( $params, qw( assay result created_by created_at ) ) },
         $self->pspec_add_well_assay_result
@@ -201,8 +202,8 @@ sub add_well_assay_result {
 
     if ( $well->assay_complete ) {
         $self->throw( InvalidState => 'Assay results cannot be added to a well in state assay_complete' );
-    }    
-    
+    }
+
     my $assay_result = $well->create_related( well_assay_results => $validated_params );
 
     unless ( $well->assay_pending and $well->assay_pending <= $assay_result->created_at ) {
@@ -212,6 +213,129 @@ sub add_well_assay_result {
     return $assay_result;
 }
 
+=head1 Delete Well
+
+Delete a well, its linked data  and all its corresponding process
+
+=cut
+sub delete_well {
+    my ( $self, $well ) = @_;
+    $self->log->debug('deleting well: ' . $well->well_name );
+
+    my $process_linked_multiple_wells = $self->_check_process_linked_to_multiple_well( $well );
+
+    $self->_check_tree_paths( $well );
+    $self->_check_process( $well );
+    $self->_delete_sub_process( $well ) unless $process_linked_multiple_wells;
+    $self->_delete_well( $well );
+
+
+    $self->_delete_process( $well ) unless $process_linked_multiple_wells;
+}
+
+sub _delete_well {
+    my ( $self, $well ) = @_;
+
+    $self->log->debug('.. deleting well and its related data');
+    my @related_well_data = qw(
+        tree_paths_ancestors
+        tree_paths_descendants
+        well_legacy_qc_test_result
+        well_assay_results
+        well_accepted_override
+    );
+
+    for my $related_data_type ( @related_well_data ) {
+        my $related_data_rs = $well->$related_data_type;
+        if ( $related_data_rs ) {
+            $related_data_rs->delete;
+        }
+    }
+
+    $well->delete;
+}
+
+sub _check_process_linked_to_multiple_well {
+    my ( $self, $well ) = @_;
+
+    my $linked_wells_rs = $well->process->wells;
+    if ( $linked_wells_rs->count == 1 ) {
+        return;
+    }
+    else {
+        my @well_names = map { $_->well_name } $linked_wells_rs->all;
+        $self->log->info(' .. not deleting process, linked to other well: ' . join(',', @well_names) );
+        return 1;
+    }
+}
+
+sub _check_tree_paths {
+    my ( $self, $well ) = @_;
+
+    # check for well descendants
+    my $descendants_rs = $well->tree_paths_ancestors->search_rs( { path_length => { '>', 0 } } );
+    if ( $descendants_rs->count ) {
+        LIMS2::Model::Error::Database->throw(
+            sprintf 'Well %s (%d) has descendant wells, cannot delete',
+            $well->well_name, $well->well_id
+        );
+    }
+}
+
+sub _check_process {
+    my ( $self, $well ) = @_;
+
+    #the tree paths check might catch everything, not sure, but lets check the
+    #processes we know are linked to wells to be sure
+    my @processes = qw( process_2w_gateways process_3w_gateways process_int_recoms process_rearray_source_wells );
+
+    for my $process ( @processes ) {
+        my $process_rs = $well->$process;
+        if ( $process_rs->count > 0 ) {
+            LIMS2::Model::Error::Database->throw( sprintf 'Well %s (%d) has a %s linked to it, cannot delete',
+                                                  $well->well_name, $well->well_id, $process );
+        }
+    }
+}
+
+sub _delete_sub_process {
+    my ( $self, $well ) = @_;
+
+    #delete the process and any rows tied into this
+    my $process = $well->process;
+    my $process_type = $process->process_type;
+    my $sub_process = $process->get_process;
+
+    if ( $process_type eq 'rearray' ) {
+        $self->log->debug('.. deleting rearray process');
+        $sub_process->process_rearray_source_wells->delete;
+        $sub_process->delete; # can we get this to cascade delete the process_rearray_source_wells
+    }
+    elsif ( $process_type eq 'int_recom' ) {
+        $self->log->debug('.. deleting int_recom process');
+        $sub_process->delete;
+    }
+    else {
+        LIMS2::Model::Error::Database->throw( sprintf 'Well %s (%d) is from a %s, unable to delete yet',
+                                              $well->well_name, $well->well_id, $process_type );
+    }
+}
+
+sub _delete_process {
+    my ( $self, $well ) = @_;
+
+    my $process = $well->process;
+    $self->log->debug('.. deleting process');
+    my $process_pipeline = $process->process_pipeline;
+    $process_pipeline->delete if $process_pipeline;
+
+    #check and delete any synthetic_construct processes
+    if ( my $synth_construct_process = $process->process_synthetic_construct ) {
+        $synth_construct_process->delete;
+    }
+
+    $process->delete;
+}
 1;
 
 __END__
